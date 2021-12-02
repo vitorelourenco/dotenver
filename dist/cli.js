2c3dceaa8cdf512567538b51f0f4d258496f931a
Vitor Lourenco <vitor.registros@lourencos.net>
2021-12-02 13:11:14 -0300

    Upload dist

--- dist/cli.js
+++ dist/cli.js
@@ -6,8 +6,14 @@ exports.__esModule = true;
 var index_1 = require("./index");
 var minimist_1 = __importDefault(require("minimist"));
 var args = (0, minimist_1["default"])(process.argv.slice(2));
-var path = args.path || index_1.defaultConfig.path;
-var encoding = args.encoding || index_1.defaultConfig.options.encoding;
-var config = { options: { encoding: encoding }, path: path };
-(0, index_1.generate)(config);
+var _ = args._;
+if (_.includes("prepare")) {
+    var source = args.s;
+    (0, index_1.generateTempYaml)(source);
+}
+else {
+    var encoding = args.encoding || index_1.defaultConfig.options.encoding;
+    var config = { options: { encoding: encoding } };
+    (0, index_1.generateDotEnv)(config);
+}
 //# sourceMappingURL=cli.js.map
